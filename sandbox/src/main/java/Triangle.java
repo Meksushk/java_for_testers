81ddaa3c340fffc14b7b0ba2f47390ab680c7379
Ksenia Bolotskikh <madam.bolotsckix2017@yandex.ru>
2025-03-03 18:14:34 +0300

    Задание 2. Создан класс Triangle с тремя свойствами.

---
+++ sandbox/src/main/java/Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle {
+
+    public double a;
+    public double b;
+    public double c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
